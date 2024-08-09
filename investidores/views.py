4a556869015c96194126d19ea650878bbdd57692
Luiz-ROCampos <ricardoxluiz88@gmail.com>
2024-08-09 17:10:37 -0300

    Commit - Atualização da pagina sugestao, criado pagiina ver empresa

--- investidores/views.py
+++ investidores/views.py
@@ -1,12 +1,16 @@
-from django.shortcuts import render
-from empresarios.models import Empresas
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from empresarios.models import Empresas,Documento
 
 # Create your views here.
+
 def sugestao(request):
+    if not request.user.is_authenticated:
+        return redirect('/usuarios/logar')
     areas = Empresas.area_choices
     if request.method == "GET":   
         return render(request, 'sugestao.html', {'areas': areas})
-    elif request.method == "POSt": 
+    elif request.method == "POST":
         tipo = request.POST.get('tipo')
         area = request.POST.getlist('area')
         valor = request.POST.get('valor')
@@ -14,14 +18,20 @@ def sugestao(request):
         if tipo == 'C':
             empresas = Empresas.objects.filter(tempo_existencia='+5').filter(estagio="E")
         elif tipo == 'D':
-            empresas = Empresas.objects.filter(tempo_existencia__in=['-6', '+6', '+1']).exclude(estagio="E")  
-
-    empresas = empresas.filter(area__in=area)
+            empresas = Empresas.objects.filter(tempo_existencia__in=['-6', '+6', '+1']).exclude(estagio="E")
+        #TODO: Criar tipo generico
+        empresas = empresas.filter(area__in=area)
         
-    empresas_selecionadas = []
-    for empresa in empresas:
-        percentual = (float(valor) * 100) / float(empresa.valuation)
-        if percentual >= 1:
-            empresas_selecionadas.append(empresa)
+        empresas_selecionadas = []
+        for empresa in empresas:
+            percentual = (float(valor) * 100) / float(empresa.valuation)
+            if percentual >= 1:
+                empresas_selecionadas.append(empresa)
 
-    return render(request, 'sugestao.html', {'empresas': empresas_selecionadas, 'areas': areas})
+        return render(request, 'sugestao.html', {'empresas': empresas_selecionadas, 'areas': areas})
+
+def ver_empresa(request, id):
+    empresa = Empresas.objects.get(id=id)
+    documentos = Documento.objects.filter(empresa=empresa)
+    #TODO: Filtrar as metricas dinamicamente
+    return render(request, 'ver_empresa.html', {'empresa': empresa, 'documentos': documentos})
